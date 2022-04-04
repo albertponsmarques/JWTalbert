6a2a2b2ac2f7f8eddf07afae2e88d31d4f4fd3b1
albert.pons.7e4 <albert.pons.7e4@itb.cat>
2022-04-04 18:45:20 +0200

    initial commit

---
+++ src/test/java/com/example/jwtalbert/JwTalbertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtalbert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwTalbertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
